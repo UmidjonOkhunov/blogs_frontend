70f6f8e0709640fe134f8072a2aa8b9620fb3b9f
UmidjonOkhunov <umidjon@kaist.ac.kr>
2020-07-16 16:45:14 +0900

    Show information about the blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
 
-export default Blog
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: "solid",
+  borderWidth: 1,
+  marginBottom: 5,
+};
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const showWhenVisible = { display: visible ? "" : "none", "line-height": 1 };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}{" "}
+        <button onClick={toggleVisibility}>{visible ? "Hide" : "Show"}</button>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>{blog.likes}</p>
+        <p>{blog.author}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
